24c865ecc70c803d635e95476782882daf4e2621
Hasib khan <khanhasib8708.code@gmail.com>
2026-09-04 14:48:03 +0530

    closures

---
+++ lecture 37- Closures/hoisting .js
@@ -0,0 +1,44 @@
+
+// function outter() {
+//     let a = 5;
+//     function inner() {
+//         console.log(a);
+//         let a = 7;
+//     }
+//     inner()
+// }
+// outter()
+
+
+
+
+// function outter() {
+//     let a = 5;
+//     function inner() {
+//         console.log(a);
+//     }
+//     return inner;
+// }
+// const response= outter();
+// response();
+
+
+
+// function outter() {
+//     let count = 0;
+//     function counter() {
+//         count = count + 1;
+//         console.log(count);
+
+//     }
+//     return counter;
+// }
+// const counter1 = outter();
+// const counter2 = outter();
+// counter1()
+// counter1()
+// counter2()
+// counter2()
+// counter2()
+
+
